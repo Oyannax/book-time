e678db7f5eba0c4ef503b972c98f442c4e701bec
Oyannax <oyannalopez14@gmail.com>
2024-01-29 16:32:54 +0100

    content-type issue resolved

--- api.php
+++ api.php
@@ -3,101 +3,118 @@ require_once './vendor/autoload.php';
 require_once './includes/_functions.php';
 include './includes/_db.php';
 
-header('content-type:application/json');
-$data = json_decode(file_get_contents('php://input'), true);
-
+// header('content-type:application/json');
 session_start();
-if (!isset($data['action'])) throwAsyncError('Aucune action.');
 
-checkCSRFAsync($data);
-checkXSS($data);
+$contentType = isset($_SERVER['CONTENT_TYPE']) ? trim($_SERVER['CONTENT_TYPE']) : '';
 
-// REGISTER
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && $data['action'] === 'register' && isset($data['username']) && isset($data['email']) && isset($data['password']) && isset($data['passwordCheck'])) {
-    // All fields completed?
-    if (strlen($data['username']) <= 0) throwAsyncError('Veuillez saisir un nom d\'utilisateur.');
-    if (strlen($data['email']) <= 0) throwAsyncError('Veuillez saisir une adresse mail.');
-    if (strlen($data['password']) <= 0) throwAsyncError('Veuillez saisir un mot de passe.');
-    if (strlen($data['passwordCheck']) <= 0 || $data['password'] !== $data['passwordCheck']) throwAsyncError('Veuillez confirmer le mot de passe.');
+if ($contentType === 'application/json') {
+    $data = json_decode(file_get_contents('php://input'), true);
 
-    // Valid email?
-    $email = filter_var($data['email'], FILTER_SANITIZE_EMAIL);
-    if (!isEmailValid($data['email'])) throwAsyncError('Veuillez saisir une adresse mail valide.');
+    if (!isset($data['action'])) throwAsyncError('Aucune action.');
 
-    // Valid password?
-    if (!isPwdValid($data['password'])) throwAsyncError('Veuillez saisir un mot de passe valide.');
+    checkCSRFAsync($data);
+    checkXSS($data);
 
-    try {
-        $query = $dbCo->prepare('SELECT user_email FROM profile WHERE user_email = :email;');
-        $query->execute([
-            'email' => $email
-        ]);
-        $result = $query->fetchAll();
-        if (isset($result[0])) throwAsyncError('Cette adresse mail existe déjà.');
+    // REGISTER
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && $data['action'] === 'register' && isset($data['username']) && isset($data['email']) && isset($data['password']) && isset($data['passwordCheck'])) {
+        // All fields completed?
+        if (strlen($data['username']) <= 0) throwAsyncError('Veuillez saisir un nom d\'utilisateur.');
+        if (strlen($data['email']) <= 0) throwAsyncError('Veuillez saisir une adresse mail.');
+        if (strlen($data['password']) <= 0) throwAsyncError('Veuillez saisir un mot de passe.');
+        if (strlen($data['passwordCheck']) <= 0 || $data['password'] !== $data['passwordCheck']) throwAsyncError('Veuillez confirmer le mot de passe.');
 
-        $register = $dbCo->prepare('INSERT INTO profile (username, user_email, user_password) VALUES (:username, :email, :password);');
-        $isRegisterOk = $register->execute([
-            'username' => $data['username'],
-            'email' => $email,
-            'password' => password_hash($data['password'], PASSWORD_DEFAULT)
-        ]);
+        // Valid email?
+        $email = filter_var($data['email'], FILTER_SANITIZE_EMAIL);
+        if (!isEmailValid($data['email'])) throwAsyncError('Veuillez saisir une adresse mail valide.');
 
-        if ($isRegisterOk) {
-            $_SESSION['id_profile'] = $dbCo->lastInsertId();
-            throwAsyncMsg('Votre compte a été créé avec succès ! Redirection...');
+        // Valid password?
+        if (!isPwdValid($data['password'])) throwAsyncError('Veuillez saisir un mot de passe valide.');
+
+        try {
+            $query = $dbCo->prepare('SELECT user_email FROM profile WHERE user_email = :email;');
+            $query->execute([
+                'email' => $email
+            ]);
+            $result = $query->fetchAll();
+            if (isset($result[0])) throwAsyncError('Cette adresse mail existe déjà.');
+
+            $register = $dbCo->prepare('INSERT INTO profile (username, user_email, user_password) VALUES (:username, :email, :password);');
+            $isRegisterOk = $register->execute([
+                'username' => $data['username'],
+                'email' => $email,
+                'password' => password_hash($data['password'], PASSWORD_DEFAULT)
+            ]);
+
+            if ($isRegisterOk) {
+                $_SESSION['id_profile'] = $dbCo->lastInsertId();
+                throwAsyncMsg('Votre compte a été créé avec succès ! Redirection...');
+            }
+        } catch (Exception $e) {
+            throwAsyncError('Une erreur s\'est produite lors de la création du compte.' . $e->getMessage());
         }
-    } catch (Exception $e) {
-        throwAsyncError('Une erreur s\'est produite lors de la création du compte.' . $e->getMessage());
     }
-}
 
 
-// LOGIN
-else if ($_SERVER['REQUEST_METHOD'] === 'POST' && $data['action'] === 'login' && isset($data['email']) && isset($data['password'])) {
-    // All fields completed?
-    if (strlen($data['email']) <= 0) throwAsyncError('Veuillez saisir une adresse mail.');
-    if (strlen($data['password']) <= 0) throwAsyncError('Veuillez saisir un mot de passe.');
+    // LOGIN
+    else if ($_SERVER['REQUEST_METHOD'] === 'POST' && $data['action'] === 'login' && isset($data['email']) && isset($data['password'])) {
+        // All fields completed?
+        if (strlen($data['email']) <= 0) throwAsyncError('Veuillez saisir une adresse mail.');
+        if (strlen($data['password']) <= 0) throwAsyncError('Veuillez saisir un mot de passe.');
 
-    try {
-        $login = $dbCo->prepare('SELECT id_profile, user_password FROM profile WHERE user_email = :email;');
-        $isLoginOk = $login->execute([
-            'email' => $data['email']
-        ]);
+        try {
+            $login = $dbCo->prepare('SELECT id_profile, user_password FROM profile WHERE user_email = :email;');
+            $isLoginOk = $login->execute([
+                'email' => $data['email']
+            ]);
 
-        if ($isLoginOk) {
-            $result = $login->fetchAll();
+            if ($isLoginOk) {
+                $result = $login->fetchAll();
 
-            // Existing account?
-            if (!isset($result[0])) throwAsyncError('Cette adresse mail n\'existe pas.');
-            if (!password_verify($data['password'], $result[0]['user_password'])) throwAsyncError('Votre mot de passe est incorrect.');
+                // Existing account?
+                if (!isset($result[0])) throwAsyncError('Cette adresse mail n\'existe pas.');
+                if (!password_verify($data['password'], $result[0]['user_password'])) throwAsyncError('Votre mot de passe est incorrect.');
 
-            // If yes
-            $_SESSION['id_profile'] = $result[0]['id_profile'];
-            throwAsyncMsg('Vous êtes bien connecté(e) ! Redirection...');
+                // If yes
+                $_SESSION['id_profile'] = $result[0]['id_profile'];
+                throwAsyncMsg('Vous êtes bien connecté(e) ! Redirection...');
+            }
+        } catch (Exception $e) {
+            throwAsyncError('Une erreur s\'est produite lors de la connexion au compte.' . $e->getMessage());
         }
-    } catch (Exception $e) {
-        throwAsyncError('Une erreur s\'est produite lors de la connexion au compte.' . $e->getMessage());
     }
-}
+} else if (strpos($contentType, 'multipart/form-data') !== false) {
+    $formData = $_POST;
 
+    // ADD
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && $formData['action'] === 'add' && isset($formData['title'])) {
+        // All fields completed?
+        if (strlen($formData['title']) <= 0) {
+            header('content-type:application/json');
 
-// ADD
-else if ($_SERVER['REQUEST_METHOD'] === 'POST' && $data['action'] === 'add' && isset($data['title'])) {
-    // All fields completed?
-    if (strlen($data['title']) <= 0) throwAsyncError('Veuillez saisir le titre du livre.');
+            throwAsyncError('Veuillez saisir le titre du livre.');
+        }
 
-    // Valid image upload?
-    // if (strlen($data['cover']) > 0) {
-    //     if (!isset($_FILES['image'])) throwAsyncError('Aucun fichier trouvé.');
+        // Valid image upload?
+        // if (strlen($formData['cover']) > 0) {
+        //     if (!isset($_FILES['image'])) throwAsyncError('Aucun fichier trouvé.');
 
-    //     $filePath = $_FILES['image']['tmp_name'];
-    //     $fileSize = filesize($filePath);
-    //     $fileInfo = finfo_open(FILEINFO_MIME_TYPE);
-    //     $fileType = finfo_file($fileInfo, $filePath);
-    // };
+        //     $filePath = $_FILES['image']['tmp_name'];
+        //     $fileSize = filesize($filePath);
+        //     $fileInfo = finfo_open(FILEINFO_MIME_TYPE);
+        //     $fileType = finfo_file($fileInfo, $filePath);
+        // }
 
-    // Valid ISBN?
-    if (strlen($data['isbn']) > 0) {
-        if (!preg_match('@(?=.*\d).{13}|(?=.*\d).{10}@', $data['isbn'])) throwAsyncError('Votre ISBN doit comporter soit 10 ou 13 chiffres.');
-    };
+        // Valid ISBN?
+        if (strlen($formData['isbn']) > 0) {
+            if (!preg_match('@(?=.*\d).{13}|(?=.*\d).{10}@', $formData['isbn'])) {
+                header('content-type:application/json');
+
+                throwAsyncError('Votre ISBN doit comporter soit 10 ou 13 chiffres.');
+            }
+        }
+
+        header('content-type:application/json');
+        throwAsyncMsg('Formulaire traité avec succès.');
+    }
 }
--- assets/js/add.js
+++ assets/js/add.js
@@ -2,37 +2,36 @@ import * as utils from './functions';
 
 document.getElementById('addForm').addEventListener('submit', function (event) {
     event.preventDefault();
-    const data = {
-        action: 'add',
-        token: utils.getToken(),
-        cover: this.querySelector('input[name="image"]').value,
-        title: this.querySelector('input[name="title"]').value,
-        author: this.querySelector('input[name="author"]').value,
-        editor: this.querySelector('input[name="editor"]').value,
-        isbn: this.querySelector('input[name="isbn"]').value,
-        size: this.querySelector('input[name="size"]').value,
-        summary: this.querySelector('textarea[name="summary"]').value
-    };
-    console.log(data);
 
-    if (data.token.length < 1) {
+    const formData = new FormData(this);
+    const headers = new Headers();
+
+    console.log(formData);
+
+    if (utils.getToken().length < 1) {
         utils.displayError('error_token');
         return;
     }
-    if (data.title.length < 1) {
+    if (formData.get('title').length < 1) {
         utils.displayError('Veuillez saisir le titre du livre.');
         return;
     }
 
-    utils.fetchAPI('POST', data)
+    fetch('api.php', {
+        method: 'POST',
+        body: formData,
+        headers: headers
+    })
+        .then(response => response.json())
         .then(response => {
             console.log(response);
+
             if (!response.result) {
                 utils.displayError(response.error);
                 return;
             }
+
             utils.displayMsg(response.msg);
-            // setTimeout(() => document.location.replace('books.php'), 1500);
             return;
         })
 })
