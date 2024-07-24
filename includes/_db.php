d07193ba01edf9476a667e920eab388903f01cae
Oyannax <oyannalopez14@gmail.com>
2024-07-24 12:22:38 +0200

    fix db connection & add .env support

--- includes/_db.php
+++ includes/_db.php
@@ -3,11 +3,13 @@ try {
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
     $dotenv->load();
 
-    $dbCo = new PDO(
-        $_ENV['DB_HOST'],
-        $_ENV['DB_USER'],
-        $_ENV['DB_PWD']
-    );
+    $dbHost = $_ENV['DB_HOST'];
+    $dbDatabase = $_ENV['DB_DATABASE'];
+    $dbUser = $_ENV['DB_USER'];
+    $dbPwd = $_ENV['DB_PWD'];
+
+    $dsn = "mysql:host=$dbHost;dbname=$dbDatabase";
+    $dbCo = new PDO($dsn, $dbUser, $dbPwd);
     $dbCo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (Exception $e) {
     die('Unable to connect to the database.' . $e->getMessage());
